e7fd8aa0b9723c14bbaffaf5066177a4f3bede6f
Kamil Polanski <polanski88@gmail.com>
2019-08-28 18:06:35 +0200

    End of task

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   'GOOGLE_CLIENT_ID': '273062760315-fahopl4u1kiklj1cboau28krhq6tfooh.apps.googleusercontent.com',
   'GOOGLE_CLIENT_SECRET': '-dH5I10f9mXd0hDCMUJWC7W5', 
-  'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+  'CALLBACK_URL': 'http://localhost:3005/auth/google/callback'
 }
